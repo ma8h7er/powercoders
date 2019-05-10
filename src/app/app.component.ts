313e15ff01124e8045545e4bf03bd75225f257a3
maher <ma8h7er@gmail.com>
2019-05-10 15:03:28 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pow-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'powercoders';
+}
